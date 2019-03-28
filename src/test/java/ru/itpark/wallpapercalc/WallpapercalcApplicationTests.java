bf9f5721396d1c859aa1f30c1f5abb74e01e76c2
idris-musin <idris-musin@localhost>
2019-03-28 23:27:13 +0300

    WallpapercalcApplicationTests is added

--- src/test/java/ru/itpark/wallpapercalc/WallpapercalcApplicationTests.java
+++ src/test/java/ru/itpark/wallpapercalc/WallpapercalcApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.itpark.wallpapercalc.service.WallpapercalcService;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,9 @@ public class WallpapercalcApplicationTests {
 
     @Test
     public void contextLoads() {
+        WallpapercalcService service = new WallpapercalcService();
+        int result = (int)service.calculate(3.59, 9.83, 4.35);
+        assertEquals(14,result);
     }
 
 }
